3d2cae208bce5f565ef04e3d268d2610a07d5bcb
sivareddyvara <sivaready999@gmail.com>
2024-01-22 11:48:47 +0530

    modification of program

--- list_dict_index.py
+++ list_dict_index.py
@@ -1,9 +1,9 @@
 def siva(x):
-    if isinstance(x, list):
+    if not isinstance(x, list):
        return x
     d={}
-    for i,j in enumarate(x):
-        d[i]=siva[j]
+    for i,j in enumerate(x):
+        d[i]=siva(j)
     return d
         
 x=[1,2,3,4,[5,6,7,8,[9,10,11,12]]]
